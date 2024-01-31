b1a0b574a20edfb6d7881cfbc01cea4f53dcc4b4
pivas0v <150503384+pivas0v@users.noreply.github.com>
2024-01-31 18:16:48 +1000

    pivasov

---
+++ 31.01.2024.ts
@@ -0,0 +1,22 @@
+class ClassName {}
+class Point { x: number; y: number; }
+let qwerty: Point = new Point();
+
+function qwerty1(){
+    qwerty.x =1; 
+    qwerty.y = 2;
+}
+
+qwerty1();
+
+class Point1 {width: number; height: number};
+let qwerty2: Point1 = new Point1()
+
+function qwerty3(){
+    qwerty2.height = 5;
+    qwerty2.width = 10;
+}
+
+qwerty3();
+
+
